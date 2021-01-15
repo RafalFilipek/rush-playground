ebb905028e406e1ea69be11f1e246e165254d468
Rafał Filipek <rafal.filipek@orange.com>
2021-01-15 17:45:25 +0100

    up stuff

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  babelrcRoots: [".", "libs/opl/*"],
+};
+
+console.log("root");
---
+++ libs/opl/ui/.babelrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+        importSource: '@emotion/react',
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+};
